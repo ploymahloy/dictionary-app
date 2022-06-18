0a3b0f658bfa0f71927b270de3cb9221309a50f2
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-06-18 15:03:15 -0400

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
 	return (
     <div className="Navbar">
       <div className="logo">
-        <img src={logo} className="logo-img"/>
+        <img src={logo} alt="..." className="logo-img"/>
         <h1 className="logo-title">Basiclopedia</h1>
       </div>
 		</div>
